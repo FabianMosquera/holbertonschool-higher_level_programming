58a505641aee608b68e21fd326f519eacf596fc4
FabianMosquera <rfmq9530@gmail.com>
2020-04-28 18:07:49 -0500

    new node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-/*
+
+/**
  * insert_node - inserts new node to linked list
- * @head: head of linked list
+ * @head: head of singly linked list
  * @number: input value
- * Return: address
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *n_node, *tmp;
+	listint_t *tmp, *n_node;
 
-	tmp = *head;
 	if (!head)
 		return (NULL);
-	n_node = malloc(sizeof(listint_t));
-	if (!n_node)
+
+	n_node = malloc(sizeof(listint_t *));
+	if (n_node == NULL)
 		return (NULL);
+	n_node->next = NULL;
 	n_node->n = number;
-	if (!tmp || tmp->n >= number)
+
+	tmp = *head;
+	while (tmp)
 	{
-		n_node->next = tmp;
-		*head = n_node;
-		return (n_node);
+		if (tmp->n >= number)
+		{
+			n_node->next = tmp;
+			*head = n_node;
+			return (n_node);
+		}
+		else if (tmp->n <= number && tmp->next->n >= number)
+		{
+			if (tmp->next != NULL)
+			{
+				n_node->next = tmp->next;
+				tmp->next = n_node;
+				return (n_node);
+			}
+		}
+		tmp = tmp->next;
 	}
-	return (n_node);
+	return (NULL);
 }
