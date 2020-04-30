c283a96aee0f8a3cf757b7a2e86f23d0eace1d23
FabianMosquera <rfmq9530@gmail.com>
2020-04-30 20:09:14 +0000

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for str in range(len(dir(hidden_4))):
-        if dir(hidden_4)[str][:2] != "__":
-            print("{:s}".format(dir(hidden_4)[str]))
+    import sys
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
+
