71fa3e13a8a4bfd82478963b490183e8694184d9
FabianMosquera <rfmq9530@gmail.com>
2020-06-02 23:39:58 +0000

    fix advanced

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,12 +7,12 @@ My integer
 class MyInt(int):
     """MyInt that inherits from int
     """
-    def __equal__(self, value):
+    def __eq__(self, value):
         """ equal (=) inverted
         """
-        return False
+        return super().__ne__(value)
 
-    def __nequal__(self, value):
+    def __ne__(self, value):
         """ not equal (!=) inverted
         """
-        return True
+        return super().__eq__(value)
