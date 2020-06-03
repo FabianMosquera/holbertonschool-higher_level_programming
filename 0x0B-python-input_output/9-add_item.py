5a518ff15d944c7b6f9077f83e40a83b68e0abbe
FabianMosquera <rfmq9530@gmail.com>
2020-06-03 23:03:53 +0000

    fix problem

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,14 +3,17 @@
 Load, add, save
 """
 import sys
+import os
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    lf = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    lf = []
-argc = len(sys.argv)
-for idx in range(1, argc):
-    lf.append(sys.argv[idx])
-save_to_json_file(lf, "add_item.json")
+if __name__ == "__main__":
+    data = "add_item.json"
+    if os.path.isfile(data):
+        save_file = load_from_json_file(data)
+    else:
+        save_file = []
+    for i in range(1, len(sys.argv)):
+        save_file.append(sys.argv[i])
+    save_to_json_file(save_file, data)
+    
