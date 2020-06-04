9a758f05e28e8f3f985ac042b94cc05f9e2a7be6
FabianMosquera <rfmq9530@gmail.com>
2020-06-04 00:12:28 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Student to JSON with filter
+"""
+
+
+class Student:
+    """defines a student by: (based on 11-student.py)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary of Student with conditions to filter
+        """
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            tmp = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+                if i in self.__dict__.keys():
+                    tmp[i] = self.__dict__[i]
+            return tmp
+
+    def reload_from_json(self, json):
+        """Replaces all items in `json`
+        """
+        for items in json.keys():
+            self.__dict__[items] = json[items]
