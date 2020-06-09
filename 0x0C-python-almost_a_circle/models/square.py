ccf49079685ce0d17b0c022c2cd124e741029cc2
FabianMosquera <rfmq9530@gmail.com>
2020-06-09 22:00:08 +0000

    write again

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Square
+"""square
 """
 from models.rectangle import Rectangle
 
@@ -10,17 +9,14 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
-        self.size = size
 
     @property
     def size(self):
-        """Getter size
-        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter size
+        """size needs to be an int
         """
         self.width = value
         self.height = value
@@ -34,8 +30,8 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for k, v in kwargs.items():
+                setattr(self, k, v)
         elif len(args) != 0:
             try:
                 self.id = args[0]
@@ -48,6 +44,6 @@ class Square(Rectangle):
             print()
 
     def to_dictionary(self):
-        """A dictionary
+        """Returns a dict representation
         """
         return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
