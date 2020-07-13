020b6bee3e0862352ef98b2854e3204dbd46413a
FabianMosquera <rfmq9530@gmail.com>
2020-07-13 18:07:20 +0000

    Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Script that lists all records with a score >= 10
+-- display both the score and the name.
+-- ordered by score.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
