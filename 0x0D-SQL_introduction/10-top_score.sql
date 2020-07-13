295b5f3b600a5c94c55987886d3597ef0c0caad3
FabianMosquera <rfmq9530@gmail.com>
2020-07-13 18:06:54 +0000

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script that lists all records of the table second_table
+-- display both the score and the name
+-- ordered by score
+-- database name will be passed as an argument
+SELECT score, name FROM second_table ORDER BY score DESC;
