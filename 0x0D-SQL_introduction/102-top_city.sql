7f35aa210b54bc3aea0e83c7933d220503f5abfb
FabianMosquera <rfmq9530@gmail.com>
2020-07-13 23:44:11 +0000

    advanced

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- script that displays the top 3 of cities temperature.
+SELECT `city`, AVG(`value`) AS avg_temp
+FROM temperatures
+WHERE `month` > 6 AND `month` < 9
+GROUP BY `city`
+ORDER BY avg_temp DESC
+LIMIT 3;
