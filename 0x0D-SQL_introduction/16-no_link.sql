f99afdb74b6792a1f5ad34a58668ebac5d4888f7
FabianMosquera <rfmq9530@gmail.com>
2020-07-13 18:00:30 +0000

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Script that lists all records of the table
+-- Don’t list rows without a name value
+-- display the score and the name
+-- listed by descending score
+SELECT score , name 
+FROM second_table WHERE name
+IS NOT NULL ORDER BY score DESC;
