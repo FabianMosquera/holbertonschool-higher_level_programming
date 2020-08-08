851b53c173f8409c4d435f63f030a317720ff807
FabianMosquera <rfmq9530@gmail.com>
2020-08-08 00:05:20 +0000

    filter names

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def get_names():
+    """ Arguments argv to connect to database
+    argv[1]: mysql username
+    argv[2]: mysql password
+    argv[3]: database name
+    argv[4]: state name searched
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY '{}' ORDER BY id ASC"""
+                .format(sys.argv[4]))
+    # fetchall is necesary for that the print show as a tuple
+    var = cur.fetchall()
+    for i in var:
+        print(i)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    get_names()
