b703d6bd075cb9d1cf465e076a3960a72f5dff19
FabianMosquera <rfmq9530@gmail.com>
2020-08-08 16:03:14 +0000

    fix pep8

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,10 +12,10 @@ def get_names_save():
     argv[4]: state name searched
     """
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
