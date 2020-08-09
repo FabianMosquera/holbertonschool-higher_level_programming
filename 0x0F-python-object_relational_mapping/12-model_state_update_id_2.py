df2d67376008a56d6322ceefb9f94f93f06f162d
FabianMosquera <rfmq9530@gmail.com>
2020-08-09 01:51:23 +0000

    updaye the name a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""List all State objects from db"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+def update_state():
+    """ Arguments argv to connect to database
+    argv[1]: mysql username
+    argv[2]: mysql password
+    argv[3]: database name
+    argv[4]: state name to search
+    """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    to_change = session.query(State).filter(State.id == 2).first()
+
+    to_change.name = 'New Mexico'
+    session.commit()
+    session.close()
+
+if __name__ == "__main__":
+    update_state()
