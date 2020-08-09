da4297a9513be0aadcc55c5d21f044e6e47b1b12
FabianMosquera <rfmq9530@gmail.com>
2020-08-09 01:20:34 +0000

    chage the form ofprint

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,32 +1,21 @@
+
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+""" Python Project """
+
 import MySQLdb
-import sys
+from sys import argv
 
 
-def get_names_save():
-    """ Arguments argv to connect to database
-    argv[1]: mysql username
-    argv[2]: mysql password
-    argv[3]: database name
-    argv[4]: state name searched
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
+if __name__ == "__main__":
+    """ lists all the states from the database"""
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         host="localhost", port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY %s ORDER BY id ASC""",
                 (argv[4], ))
-    # fetchall is necesary for that the print show as a tuple
-    var = cur.fetchall()
-    for i in var:
+    content = cur.fetchall()
+    for i in content:
         print("{}".format(i))
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    get_names_save()
