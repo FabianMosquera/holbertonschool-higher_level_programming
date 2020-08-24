141230541bc57cc6640941a23f954bdae1e9e300
FabianMosquera <rfmq9530@gmail.com>
2020-08-24 17:10:33 +0000

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Send POST"""
-import urllib.request
+""" Posting an email """
 import urllib.parse
-import sys
+import urllib.request
+from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
     value = {'email': email}
-
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
