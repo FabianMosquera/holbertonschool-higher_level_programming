135c1f8f00890a422aaa88dcab273e299f28f2c9
FabianMosquera <rfmq9530@gmail.com>
2020-09-15 17:46:07 +0000

    [Fix: semistandard]

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // JS Print the const varibale
 
-const myVar = "Javascript is amazing";
+const myVar = 'Javascript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // JS Print only with console
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
