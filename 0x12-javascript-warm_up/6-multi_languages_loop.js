08c9075f04f5cb78ff7ca4e943c55ce1403c4b85
FabianMosquera <rfmq9530@gmail.com>
2020-09-15 18:12:21 +0000

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// JS script that prints an array
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
